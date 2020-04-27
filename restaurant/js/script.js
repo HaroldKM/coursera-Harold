c3760145bd5095cfb27cf79d975fbee289615032
HaroldKM <harodasus1230@gmail.com>
2020-04-27 10:15:04 +0200

    delivery call button

--- restaurant/js/script.js
+++ restaurant/js/script.js
@@ -1,14 +1,25 @@
 function myFunction() {
+    var w = document.getElementById("brandName");
     var x = document.getElementById("mytopnav");
     var y = document.getElementById("kosher");
     var z = document.getElementById("brand");
 
+    if(w.className === "smalfont"){
+        w.className = "";
+    }else{
+        w.className = "smalfont";
+    }
+
     if(x.className === "topnav"){
         x.className = " responsive";
-        
     }else{
-       
         x.className = "topnav";
+    }
+
+    if(y.className === "fab fa-gripfire fa-2x smalfont"){
+        y.className = " fab fa-gripfire fa-2x ";
+    }else{
+        y.className = "fab fa-gripfire fa-2x smalfont";
     }
 
     if(z.className === "navbar-brand"){
